811f05b0250ab640e5e91febbcf32a3c1934dfee
Emil Todirascu <111353538+emil-todirascu@users.noreply.github.com>
2026-03-01 19:01:11 +0100

    Revert "test(integration): install coffeescript for coffee fixture"
    This reverts commit f97a64730564570051d914d04cabba652807f9c9.

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,8 +1,4 @@
 PKG_FILE="$PWD/$(npm pack)"
 git clone https://github.com/karma-runner/integration-tests.git --depth 1
 cd integration-tests
-# Ensure the CoffeeScript fixture can load karma.conf.coffee in CI.
-if [ -d coffee ]; then
-  npm install --prefix coffee --no-save coffeescript
-fi
 ./run.sh $PKG_FILE

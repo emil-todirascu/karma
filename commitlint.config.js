6c867ff1f9d34d0f31457ddf365bd0c914205a84
Emil Todirascu <111353538+emil-todirascu@users.noreply.github.com>
2026-08-05 18:54:50 +0200

    feat: add ignore rule for Dependabot commit messages in commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-angular'] }
+const isDependabotUpdate = (message) =>
+  /(?:^|\r?\n)Signed-off-by: dependabot\[bot\] <support@github\.com>\s*$/.test(
+    message
+  )
+
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  ignores: [isDependabotUpdate]
+}

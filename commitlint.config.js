1673f1333621b6ea6bed71b87658267558edf499
Emil Todirascu <111353538+emil-todirascu@users.noreply.github.com>
2026-08-05 19:03:57 +0200

    fix: exclude Dependabot commit messages from validation in lint workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,1 @@
-const isDependabotUpdate = (message) =>
-  /(?:^|\r?\n)Signed-off-by: dependabot\[bot\] <support@github\.com>\s*$/.test(
-    message
-  )
-
-module.exports = {
-  extends: ['@commitlint/config-angular'],
-  ignores: [isDependabotUpdate]
-}
+module.exports = { extends: ['@commitlint/config-angular'] }
